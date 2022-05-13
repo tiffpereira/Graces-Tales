61b44b01e3e0b4e98dadcca39bd020e8f26e6865
tiffpereira <tiffanympereira@gmail.com>
2022-05-13 11:11:36 -0400

    reviews rendering on bookdetails page

--- client/src/pages/BookDetails.js
+++ client/src/pages/BookDetails.js
@@ -5,9 +5,10 @@ import '../styles/BookDetails.css'
 
 const BookDetails =  () => {
     let navigate = useNavigate()
-    const [bookDetails, setBookDetails] = useState([])
     let { id } = useParams()
-    console.log(id)
+
+    const [bookDetails, setBookDetails] = useState([])
+    const [reviews, setReviews] = useState([])
 
     const handleBookDetails = async () => {
         const res = await axios.get(`http://localhost:8000/books/${id}`)
@@ -15,25 +16,35 @@ const BookDetails =  () => {
         console.log(res.data)     
     }
 
+    const handleReviews = async () => {
+        const res = await axios.get(`http://localhost:8000/reviews/${id}`)
+        setReviews(res.data)
+        console.log(res.data)
+    }
+
     useEffect(() => {
         handleBookDetails()
+        handleReviews()
     }, [id])
 
-    console.log(bookDetails)
+    console.log(reviews)
 
         return (
             <div>
                 <div>
-                    <h1>BOOKS</h1>
-                </div>
-                <div>
                         <div className='individual-book'>
-                        <h3>{bookDetails.title}</h3>
-                        <img className='bookDetails-image' src={bookDetails.photo_url} alt='book cover' />
-                        <h4>{bookDetails.author}</h4>
-                        <h4>{bookDetails.genre}</h4>
-                        <h4>{bookDetails.summary}</h4>
-                        <h4>{bookDetails.price}</h4>
+                            <h3>{bookDetails.title}</h3>
+                            <img className='bookDetails-image' src={bookDetails.photo_url} alt='book cover' />
+                            <h4>{bookDetails.author}</h4>
+                            <h4>{bookDetails.genre}</h4>
+                            <h4>{bookDetails.summary}</h4>
+                            <h4>{bookDetails.price}</h4>
+                            <button onClick={() => navigate('/books')}>Back</button>
+                        </div>
+                        <div className='individual-review'>
+                           <h3>{reviews.name}</h3>
+                           <h4>{reviews.title}</h4>
+                           <h4>{reviews.body}</h4>
                         </div>
                 </div>
             </div>
